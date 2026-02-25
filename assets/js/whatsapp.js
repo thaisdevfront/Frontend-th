74a6ad7a74f8b0a8c5b4df09c102b76a36501474
Thais Mariano <86856151+thaisdevfront@users.noreply.github.com>
2026-02-25 17:39:36 -0300

    Delete assets directory

--- assets/js/whatsapp.js
+++
@@ -1,8 +0,0 @@
-function abrirWhatsApp() {
-const telefone = "5512981021517"; // DDI + DDD + número
-const mensagem = "Olá! Gostaria de mais informações";
-
-
-const url = `https://wa.me/${telefone}?text=${encodeURIComponent(mensagem)}`;
-window.open(url, "_blank");
-}
